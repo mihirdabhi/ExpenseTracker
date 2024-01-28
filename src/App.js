540384ec9b835e4e7aa77c4707a79f15f196ec90
Mihir Dabhi <mihirdabhi143@gmail.com>
2024-01-28 22:05:19 +0000

    errors solved

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 
 import './App.css';
-<<<<<<< HEAD
 import { Header} from './components/Header';
 import { Balance} from './components/Balance';
 import {IncomeExpense } from './components/IncomeExpense';
@@ -19,15 +18,7 @@ function App() {
         <AddTranscation />
       </div>
     </GlobalProvider>
-=======
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
->>>>>>> ffe64ed63c9dee6d9fd432428ae1c3feb89b0e10
-  );
-}
 
+  )}
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,29 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
+
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
- 
-    <App />
-  
-  
-);
-=======
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
->>>>>>> ffe64ed63c9dee6d9fd432428ae1c3feb89b0e10
